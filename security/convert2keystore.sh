c9b391b11d7c2d96ff4698abc6774a2174c36093
Kelsey McKenna <kelseyguitar55@googlemail.com>
2022-01-09 01:28:02 +0000

    Move convert2keystore.sh content into create_keystore.sh

--- security/convert2keystore.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-openssl pkcs12 \
-    -export \
-    -in $1-signed.pem \
-    -inkey $1.pem \
-    -out $1.p12 \
-    -name $1
-
--- security/create_keystore.sh
+++ security/create_keystore.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+openssl pkcs12 \
+    -export \
+    -in $1-signed.pem \
+    -inkey $1.pem \
+    -out $1.p12 \
+    -name $1
+
 keytool -importkeystore \
     -srcstorepass '' \
     -srckeystore $1.p12 \
